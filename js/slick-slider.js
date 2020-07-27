25085d3a656d4a47f45da92094c886b1ca1bed02
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-07-27 21:33:37 +0300

    add autoslide

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -5,6 +5,6 @@ $('.slick-slider').slick({
     slidesToShow: 1,
     centerMode: true,
     variableWidth: true,
-    // autoplay: true,
-    // autoplaySpeed: 2000
+    autoplay: true,
+    autoplaySpeed: 2000
 });
